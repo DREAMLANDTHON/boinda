5a70c058b1a24872a1af15e2a1ccc8bdf7a432b2
it794613 <it794613@naver.com>
2023-07-28 17:28:53 +0900

    레포시작

---
+++ Boinda_dreamLand/Boinda_dreamLand/Boinda_dreamLandApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boinda_dreamLandApp.swift
+//  Boinda_dreamLand
+//
+//  Created by 최진용 on 2023/07/28.
+//
+
+import SwiftUI
+
+@main
+struct Boinda_dreamLandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Boinda_dreamLand/Boinda_dreamLandUITests/Boinda_dreamLandUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Boinda_dreamLandUITestsLaunchTests.swift
+//  Boinda_dreamLandUITests
+//
+//  Created by 최진용 on 2023/07/28.
+//
+
+import XCTest
+
+final class Boinda_dreamLandUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
