5cd02dd796350070ab762d84d23639817c7fcd75
songmoro <97587585+songmoro@users.noreply.github.com>
2023-07-29 09:36:23 +0900

    feat: front connect

---
+++ Boinda_dreamLand/Boinda_dreamLand/Ext+/Color+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  Color+Ext.swift
+//  Boinda_dreamLand
+//
+//  Created by 송재훈 on 2023/07/29.
+//
+
+import SwiftUI
+
+struct Color_Ext: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Color_Ext_Previews: PreviewProvider {
+    static var previews: some View {
+        Color_Ext()
+    }
+}
